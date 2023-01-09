218b5a534e606c78435b61fa862b443e998617cd
Art3mix <r3mix.il@gmail.com>
2023-01-09 12:37:33 +0200

    continue script

--- scripts/add_liquidity.sh
+++ scripts/add_liquidity.sh
@@ -1,59 +1,86 @@
 #!/bin/bash
 
-# Run this from the root repo directory
+# Run this from the scripts directory
+
+# First argument is the Pool address, second argument is the staking contract of the same pool address.
+# Example: `./add_liquidity.sh juno1gjk9gva6dfs254qvlthw6ldqu3etp3wf2854n2ln7szepzvrggys89w3yn juno1paazsk7v2sx8tdjgw9ud22mg6hrawqwd27tjrv4nex9v529yzphsglem68`
 
 ## CONFIG
-# NOTE: you will need to update these to deploy on different network
-BINARY='junod'
+BINARY='docker exec -i cosmwasm junod'
 DENOM='ujunox'
 CHAIN_ID='uni-5'
-# RPC='http://localhost:26657/'
-# REST='http://localhost:1317/'
-# TXFLAG="--gas-prices 0.025$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID --node $RPC"
-TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID"
-# QFLAG="--chain-id $CHAIN_ID --node $RPC"
-QFLAG="--chain-id $CHAIN_ID"
-TESTER_MNEMONIC="siren window salt bullet cream letter huge satoshi fade shiver permit offer happy immense wage fitness goose usual aim hammer clap about super trend"
+RPC='https://rpc.uni.junonetwork.io:443'
+TXFLAG="--gas-prices 0.1$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID --node $RPC"
+QFLAG="--chain-id $CHAIN_ID --node $RPC"
+TESTER_MNEMONIC='siren window salt bullet cream letter huge satoshi fade shiver permit offer happy immense wage fitness goose usual aim hammer clap about super trend'
+FAUCET_MNEMONIC='siren window salt bullet cream letter huge satoshi fade shiver permit offer happy immense wage fitness goose usual aim hammer clap about super trend'
+KILL_DOCKER='docker kill cosmwasm'
 
 if [ "$1" = "" ]
 then
-  echo "Usage: $0 2 args required, LP address is missing!"
+  echo "Usage: $0 2 args required, 1st argument LP address is missing!"
+  exit
+elif [ "$2" = "" ]
+then
+  echo "Usage: $0 2 args required, 2nd argument Staking address is missing!"
   exit
 fi
 
-# LP Data, like pool address, the cw20 addresses.
+# Start docker with junod installed
+$KILL_DOCKER
+
+docker run --rm -d -t --name cosmwasm \
+    --mount type=volume,source=junod_data,target=/root \
+    --platform linux/amd64 \
+    ghcr.io/cosmoscontracts/juno:v2.1.0 sh
+
+# Try to delete faucet if already exists
+$BINARY keys delete faucet -y >/dev/null
+
+# Add faucet wallet
+echo $FAUCET_MNEMONIC | $BINARY keys add faucet --account 1 --recover
+
+$BINARY keys list
+exit
+# LP Data, like pool address, staking address, the cw20 addresses.
 POOL_ADDR=$1
+STAKING_ADDR=$2
+
 LP_RESPONSE=$($BINARY q wasm contract-state smart $POOL_ADDR '{"info":{}}' --output json $QFLAG | jq -r '.data')
 TOKEN_1_ADDR=$(echo $LP_RESPONSE | jq -r '.token1_denom | select(.cw20 != null) | .cw20')
 TOKEN_2_ADDR=$(echo $LP_RESPONSE | jq -r '.token2_denom | select(.cw20 != null) | .cw20')
 LP_TOKEN_ADDR=$(echo $LP_RESPONSE | jq -r '.lp_token_address')
 
 # echo $LP_RESPONSE
-# echo $TOKEN_1_DENOM $TOKEN_2_DENOM $LP_TOKEN_ADDR
+# echo $TOKEN_1_ADDR $TOKEN_2_ADDR $LP_TOKEN_ADDR
 
-# TODO: add a docker with junod installed to do it there instead of locally.
-
-# Read from the mnemonic file line by line
 BASE_NAME="ws-tester"
-INDEX=0
 
-for INDEX in {1..1}
+# NOTE: Important to start from index 2, because index 0 is our manual tester, index 1 is our faucet
+for INDEX in {2..2}
 do
-  # Add tester address to junod
   TESTER_NAME=$BASE_NAME"-"$INDEX
+
+  # Add tester address to junod
+  $BINARY keys delete $TESTER_NAME -y >/dev/null
   printf "y\n$TESTER_MNEMONIC" | $BINARY keys add $TESTER_NAME --account $INDEX --recover
 
-# We assume only 1 token is cw20, and the 2nd is native for now
+  # TODO: Fund wallet from faucet wallet
   if [ "$TOKEN_1_ADDR" = "" ]
   then
-    TOKEN_ADDR=$TOKEN_2_ADDR
+    # Native token, use bank to fund wallet from faucet
+
   else
-    TOKEN_ADDR=$TOKEN_1_ADDR
+    # CW20 token, use transfer to fund wallet from faucet
   fi
 
-  # TODO: How do we fund the accounts? no cli faucet or we can't do swaps if 2 CW20 tokens
-  # For native LPs, we can just swap
-  $BINARY tx wasm execute $POOL_ADDR '{"swap":{"input_token": "Token2","input_amount":"200000","min_output":"1"}}' --from $TESTER_NAME --amount 200000ujunox $TXFLAG
+  if [ "$TOKEN_2_ADDR" = "" ]
+  then
+    # Native token, use bank to fund wallet from faucet
+
+  else
+    # CW20 token, use transfer to fund wallet from faucet
+  fi
 
   # increase allowance
   $BINARY tx wasm execute $TOKEN_ADDR '{"increase_allowance":{"amount":"2000000","spender":"'"$POOL_ADDR"'"}}' --from $TESTER_NAME $TXFLAG
@@ -61,6 +88,14 @@ do
   # Add liquidity
   $BINARY tx wasm execute $POOL_ADDR '{"add_liquidity":{"token1_amount":"100","min_liquidity":"1","max_token2":"131"}}' --from $TESTER_NAME --amount 131ujunox $TXFLAG
 
+  # Stake
+  staking_msg=`echo '{"stake":{}}' | base64`
+  $BINARY tx wasm execute $LP_TOKEN_ADDR '{"send":{"contract":"'"$STAKING_ADDR"'","amount":"100","msg":"'"$staking_msg"'"}}' --from $TESTER_NAME $TXFLAG
+
   # Increase index
   let INDEX=INDEX+1
+
+  echo $TESTER_NAME" finished successfully!"
 done
+
+$KILL_DOCKER
